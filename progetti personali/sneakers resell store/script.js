90983593ac5c794208b35805ff19520dffc57a71
Elena Zen <elenazen563@gmail.com>
2025-01-25 14:59:59 +0100

    Edit SB00 jan-feb 1

--- progetti personali/sneakers resell store/script.js
+++ progetti personali/sneakers resell store/script.js
@@ -1,9 +1,22 @@
+//ham menu on mobile
 function show() {
   const menu = document.getElementById("menucont");
   const isMobile = window.matchMedia("(max-width: 860px)").matches;
 
   if (isMobile) {
-      // Se la media query è attiva, mostra/nascondi il menu
       menu.classList.toggle("openmenu");
   }
+}
+//favs and cart dropdown
+function dropdownf() {
+  const favs = document.getElementById("dropdownf");
+  const ficon = document.getElementById('favs');
+  ficon.classList.toggle("white");
+  favs.classList.toggle("openmenu");
+}
+function dropdownc() {
+  const cart = document.getElementById("dropdownc");
+  const cicon = document.getElementById('cart');
+  cicon.classList.toggle("white");
+  cart.classList.toggle("openmenu");
 }
