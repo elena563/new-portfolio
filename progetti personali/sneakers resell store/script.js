0d121c145d7158e920c8505f5fa895fa11eb0766
Elena Zen <elenazen563@gmail.com>
2025-02-26 20:58:03 +0100

    SB00 feb4 - favs and cart interactions

--- progetti personali/sneakers resell store/script.js
+++ progetti personali/sneakers resell store/script.js
@@ -40,6 +40,78 @@ document.addEventListener("click", function(event) {
   }
 });
 
+document.addEventListener("DOMContentLoaded", () => {
+  console.log("DOMContentLoaded triggerato!");
+  updateDropdown("fav");
+  updateDropdown("cart");
+});
+
+
+function showMessage(key) {
+  let message = document.getElementById(`${key}-message`);
+  console.log(message);
+  message.classList.add("show");
+  message.classList.remove("hidden");
+
+  setTimeout(() => {
+    message.classList.remove("show");
+    message.classList.add("hidden");
+  }, 3000);
+}
+
+function updateStorage(event, action, key, id) {
+  let list = JSON.parse(localStorage.getItem(key)) || [];
+  console.log(`Lista iniziale (${key}):`, list);
+  if (event) {
+    event.preventDefault();
+    event.stopPropagation();
+  }
+  console.log(list)
+
+  if (action === "add" && !list.includes(id)) {
+    list.push(id);
+    showMessage(key);
+  } else if (action === "remove") {
+    list = list.filter(prodId => prodId !== id);
+  }
+
+  localStorage.setItem(key, JSON.stringify(list));
+  updateDropdown(key);
+}
+
+
+function updateDropdown(key) {
+  let dropdownId = key === "fav" ? "dropdownf" : "dropdownc"; 
+  let dropdown = document.getElementById(dropdownId);
+  dropdown.innerHTML = "";  
+
+  let list = JSON.parse(localStorage.getItem(key)) || [];
+  console.log(list);
+  if (list.length === 0) {
+    dropdown.innerHTML = "<p>Nothing here</p>";
+    return;
+  }
+
+  list.forEach(id => {
+    let product = document.querySelector(`.product[data-id="${id}"]`);
+    if (product) {
+      let name = product.querySelector(".name").textContent;
+      let item = document.createElement("div");
+      item.classList.add("dropdown-item");
+      item.innerHTML = `<p>${name}</p> <button data-id="${id}" data-type="${key}">✖</button>`;
+
+      item.querySelector("button").addEventListener("click", (event) => {
+        updateStorage(event, "remove", key, id);
+        updateDropdown(key, dropdownId);
+      });
+
+      dropdown.appendChild(item);
+    }
+  });
+}
+
+
+
 //product filter
 document.addEventListener("change", function(event) {
   if (event.target.matches('.category')){
