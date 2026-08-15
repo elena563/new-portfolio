d3eac497182b3d7dcf6e72bdb31c8ffe26f9b866
Elena Zen <elenazen563@gmail.com>
2026-08-15 11:02:32 +0200

    Configure linting

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,10 +1,25 @@
 import js from '@eslint/js';
 import prettierConfig from 'eslint-config-prettier';
+import eslintPluginAstro from 'eslint-plugin-astro';
+import globals from 'globals';
 
 export default [
+  {
+    ignores: ['dist/', '.astro/', 'node_modules/'],
+  },
+
   js.configs.recommended,
   prettierConfig,
+  ...eslintPluginAstro.configs.recommended,
+
   {
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        Swiper: 'readonly',
+        dataLayer: 'writable',
+      },
+    },
     rules: {
       'no-unused-vars': 'warn',
       'no-console': 'off',
